f93fe95667e4ff5e436b192b1eca6b4d505939cd
Elijah Smith <elijahs2106@gmail.com>
2018-07-13 21:51:01 -0400

    Add functionality to remove todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("li").click((e) => {
     $(e.target).toggleClass('completed');
 });
+
+// Click on X to delete Todo
+$("span").click((e) => {
+    $(e.target).parent().fadeOut(function() {
+        $(this).remove();
+    });
+    e.stopPropagation();
+});
